18eca652ea8954602d6500e2ec87aec17ef8aedb
puma007 <hil2000@163.com>
2014-10-23 23:17:05 +0800

    html util tools

---
+++ test/HtmlImageSrcUtils.java
@@ -0,0 +1,18 @@
+package my.util;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+public class HtmlImageSrcUtils {
+	public static String getImg(String s){
+		Document doc = Jsoup.parse(s);
+		Elements imgs = doc.select("img[src]");
+		if(imgs!=null){
+			 for (Element src : imgs){
+				 return src.attr("src");
+			 }
+		}
+		return "";	
+	}	
+}
